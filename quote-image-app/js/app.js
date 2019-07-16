68c1da3469f4bc00ee5c3b8e016af44887bd66e8
Mushnik <cmorrison@Coreys-Mac.local>
2019-07-16 11:45:17 -0400

    can populate quote data

--- quote-image-app/js/app.js
+++ quote-image-app/js/app.js
@@ -1,1 +1,18 @@
 console.log('js is working');
+
+const endpoint = 'https://favqs.com/api/qotd';
+
+// console.log(endpoint);
+
+const handleData = data => {
+  console.log(data.quote.body);
+
+  const $addQuote = $('<h3>').addClass('currentQuote');
+
+  $addQuote.text(data.quote.body);
+  $('.quotes').append($addQuote);
+};
+
+$.ajax({
+  url: endpoint
+}).then(handleData);

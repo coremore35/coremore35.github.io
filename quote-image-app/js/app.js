7f02d842879a099e3e22cb0aee6999e7000bc199
Mushnik <cmorrison@Coreys-Mac.local>
2019-07-16 11:57:26 -0400

    added randomizer functionality

--- quote-image-app/js/app.js
+++ quote-image-app/js/app.js
@@ -2,8 +2,6 @@ console.log('js is working');
 
 const endpoint = 'https://favqs.com/api/qotd';
 
-// console.log(endpoint);
-
 const handleData = data => {
   console.log(data.quote.body);
 
@@ -12,6 +10,18 @@ const handleData = data => {
   $addQuote.text(data.quote.body);
   $('.quotes').append($addQuote);
 };
+
+// console.log(endpoint);
+
+$('.random').on('click', event => {
+  // event.handleData();
+
+  $('.currentQuote').remove();
+
+  $.ajax({
+    url: endpoint
+  }).then(handleData);
+});
 
 $.ajax({
   url: endpoint

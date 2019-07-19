bda902e7d7f24f33bd4038ec01894f70b786c5e5
Mushnik <cmorrison@Coreys-Mac.local>
2019-07-19 16:25:09 -0400

    corrected modal scroll

--- js/app.js
+++ js/app.js
@@ -113,6 +113,10 @@ const handleKeywordData = data => {
 
   $('form').trigger('reset');
 
+  $('.sidebar').fadeOut(1000);
+  $('#closed-sidebar').fadeIn(1000);
+  $('#logo').animate({ opacity: 0.8 }, 2000);
+
   $.ajax({
     url: imgEndpoint
   }).then(handleImg);
